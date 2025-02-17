14d81625c888c66810125faaaf44ee8ec6c37485
Marlin-ai <mlopezbaez21@gmail.com>
2025-02-17 13:32:25 -0500

    comit class Note

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,24 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+class Note:
+
+    HIGH= "HIGH"
+    MEDIUM = "MEDIUM"
+    LOW = "LOW"
+
+    def __init__(self, code: str, title: str, text: str, importance: str, datetime: str):
+        self.code:str = code
+        self.title:str = title
+        self.text:str = text
+        self.importance:str = importance
+        self.creation_date:str = datetime.now()
+        self.tags = []
+
+    def add_tag(self, tag):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self):
+        return f"date: {self.creation_date}\n{self.title}: {self.text}"
+
+class Notebook:
+
